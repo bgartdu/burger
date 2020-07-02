c2486c6b7e7f79d3bfe016196750397f04cb179b
Brandon Gart <brandongart@gmail.com>
2020-07-02 14:28:51 -0600

    modified connection js to work with Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,18 @@
 const mysql = require("mysql2/promise")
 
-module.exports = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burgerApp",
-});
+let connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burgerApp",
+    });
+}
+
+connection.connect();
+
+module.exports = connection
