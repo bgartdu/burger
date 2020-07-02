ab5895510f0b31d467fd6b4ce2411ed679782620
Brandon Gart <brandongart@gmail.com>
2020-07-02 15:10:33 -0600

    added dynamic port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const burgersController = require("./controllers/burgers_controller");
 const burgerModel = require("./models/burger");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extends: true}));
 app.use(express.json());
